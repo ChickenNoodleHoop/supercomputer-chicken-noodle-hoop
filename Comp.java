bb5541cb96a62472cb9fcf31fe941ff332037c68
jackszumowski <18szumowskij@bpcsd.org>
2016-11-02 07:39:39 -0400

    Update Comp.java

--- Comp.java
+++ Comp.java
@@ -14,19 +14,24 @@ public class Comp{
     System.out.println("Type 1 for 1 to 1000000 by 1's, type 2 for 0 to 1000000 by 5's");
     System.out.println("Type 3 for 100 down to 1 by 1's, and type 4 for 1000 to 100000 by 10's");
     String number = scan.nextLine();
-    
+    //bobby
     if(number.equals ("1")){
-    
+       for (int i = 1; i <= 1000000; i ++) {
+            System.out.print(i + " ");
     } 
+        //bobby
     else if(number.equals ("2")){
-        
+          for (int i = 0; i <= 1000000; i +=5) {
+              System.out.print(i + " ");
     }
+        //Jack
     else if(number.equals ("3") ){
 
     for(int i = 100; i >= 0; i--){
         System.out.print(i + " ");
     }
     }
+        // Jack
     else if(number.equals ("4")){
     for(int i = 1000; i <= 100000; i += 10) {
         System.out.print(i + " ");
